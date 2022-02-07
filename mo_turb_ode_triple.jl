b88f745ae020386227b485d4561e656ddb1f1084
Jia Yu <yeveshummels@gmail.com>
2022-02-07 03:07:14 +0000

    Update mo_turb_ode_triple.jl

--- mo_turb_ode_triple.jl
+++ mo_turb_ode_triple.jl
@@ -25,10 +25,13 @@ using Plots
     α::Float64 = 1.5
     g₁::Float64 = 10.0
     g₂::Float64 = 12.0
-    d₁::Float64 = 0.005
-    d₂::Float64 = 0.005
+    g₃::Float64 = 7.0
+    d₁::Float64 = 0.005   # The death rate of Chlamy
+    d₂::Float64 = 0.005   # The death rate of SC1
+    d₃::Float64 = 0.005   # The death rate of SC2
     pop_c::Float64 = 0.025
-    pop_sc::Float64 = 0.025
+    pop_sc_1::Float64 = 0.025
+    pop_sc_2::Float64 = 0.025
     Dil_const::Float64 = 0.072
     threshold::Float64 = 10.0
 end
@@ -52,7 +55,7 @@ dilution(X, Y, Z, a, b, c, d) = a * X + b * Y + c*Z + d
 # Define the ODE system, parameter list p can be retrieved from the previous settings
 function ode_sys(du, u, p, t)
     du[1] =
-        u[5] * dilution(u[3], u[4], p.pop_c, p.pop_sc, 0) * (p.N₁⁰ - u[1]) -
+        u[5] * dilution(u[3], u[4], u[5], p.pop_c, p.pop_sc_1, p.pop_sc_2, 0) * (p.N₁⁰ - u[1]) -
         p.c₁ * sub_asm(p.a, p.b, u[1]) * u[3] / p.OD_chlamy -
         p.c₂ * sub_asm(p.c_1, p.d_1, u[1]) * u[3] / p.OD_bacteria - 
         p.c₄ * sub_asm(p.c_2, p.d_2, u[1]) * u[3] / p.OD_bacteria
@@ -61,27 +64,27 @@ function ode_sys(du, u, p, t)
         p.α * u[3] -
         p.c₃ * sub_asm(p.e_1, p.f_1, u[2]) * u[4] / p.OD_bacteria -
         p.c₅ * sub_asm(p.e_2, p.f_2, u[2]) * u[4] / p.OD_bacteria -
-        u[6] * u[2] * dilution(u[3], u[4], p.pop_c, p.pop_sc, 0)
+        u[6] * u[2] * dilution(u[3], u[4], u[5], p.pop_c, p.pop_sc_1, p.pop_sc_2, 0)
 
     du[3] =
         u[3] * (
             p.c₁ * sub_asm(p.a, p.b, u[1]) -
-            u[6] * dilution(u[3], u[4], p.pop_c, p.pop_sc, 0) -
+            u[6] * dilution(u[3], u[4], u[5], p.pop_c, p.pop_sc_1, p.pop_sc_2, 0) - 
             p.d₁
         )
 
     du[4] =
         u[4] * (
-            p.c₂ * sub_asm(p.c, p.d, u[1]) +
-            p.c₃ * sub_asm(p.e, p.f, u[2]) - p.d₂ -
-            u[6] * dilution(u[3], u[4], p.pop_c, p.pop_sc, 0)
+            p.c₂ * sub_asm(p.c_1, p.d_1, u[1]) +
+            p.c₃ * sub_asm(p.e_1, p.f_1, u[2]) - p.d₂ -
+            u[6] * dilution(u[3], u[4], u[5], p.pop_c, p.pop_sc_1, p.pop_sc_2, 0)
         )
 
     du[5] = 
           u[5] * (
-            p.c₂ * sub_asm(p.c, p.d, u[1]) +
-            p.c₃ * sub_asm(p.e, p.f, u[2]) - p.d₂ -
-            u[6] * dilution(u[3], u[4], p.pop_c, p.pop_sc, 0)
+            p.c₂ * sub_asm(p.c_2, p.d_2, u[1]) +
+            p.c₃ * sub_asm(p.e_2, p.f_2, u[2]) - p.d₂ -
+            u[6] * dilution(u[3], u[4], u[5], p.pop_c, p.pop_sc_1, p.pop_sc_2, 0)
         )
 
     du[6] = 0
