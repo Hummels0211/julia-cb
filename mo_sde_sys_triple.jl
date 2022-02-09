3fb11eca32f6ee64529a6065afc7f7dfb0436718
Jia Yu <yeveshummels@gmail.com>
2022-02-09 17:29:38 +0100

    Create mo_sde_sys_triple.jl
    Create the SDE system for the triple component model

---
+++ mo_sde_sys_triple.jl
@@ -0,0 +1,166 @@
+using DifferentialEquations
+using Parameters
+using Plots
+
+# The basic turbidostat model
+
+@with_kw struct parameters
+    c₁::Float64 = 0.13   # The assimilation rate of C
+    c₂::Float64 = 0.12   # The assimilation rate of SC1
+    c₃::Float64 = 0.05   # The non-metabolic assimilation rate of SC1
+    c₄::Float64 = 0.04   # The assimilation rate of SC2
+    c₅::Float64 = 0.06   # The non-metabolic assimilation rate of SC2
+    a::Float64 = 0.95    # The assimilation constants of C
+    b::Float64 = 7.5
+    c_1::Float64 = 0.34   # The assimilation constants of SC1
+    d_1::Float64 = 8.2
+    e_1::Float64 = 0.37   # The non-metabolic assimilation constants of SC1
+    f_1::Float64 = 1.52
+    c_2::Float64 = 0.24   # The assimilation constants of SC2
+    d_2::Float64 = 10.2
+    e_2::Float64 = 0.37   # The non-metabolic assimilation constants of SC2  
+    f_2::Float64 = 1.52
+    OD_chlamy::Float64 = 1.0
+    OD_bacteria::Float64 = 1.0
+    α::Float64 = 1.5
+    β₁::Float64 = 0.005    # The Lotka Volterra coefficient from SC1 to SC2
+    β₂::Float64 = - 0.005  # The Lotka Volterra coefficient from SC2 to SC1
+    g₁::Float64 = 10.0
+    g₂::Float64 = 12.0
+    g₃::Float64 = 7.0
+    d₁::Float64 = 0.005   # The death rate of Chlamy
+    d₂::Float64 = 0.005   # The death rate of SC1
+    d₃::Float64 = 0.005   # The death rate of SC2
+    pop_c::Float64 = 0.025
+    pop_sc_1::Float64 = 0.025
+    pop_sc_2::Float64 = 0.025
+    Dil_const::Float64 = 0.072
+    threshold::Float64 = 10.0
+    N₁⁰::Float64 = 10.0
+    N₂⁰::Float64 = 0.2
+    C⁰::Float64 = 0.01
+    SC₁⁰::Float64 = 0.1
+    SC₂⁰::Float64 = 0.1
+    Δ⁰::Int = 0
+end
+
+#@with_kw struct ini_conditions
+#    N₁⁰::Float64 = 10.0
+#    N₂⁰::Float64 = 0.2
+#    C⁰::Float64 = 0.01
+#    SC₁⁰::Float64 = 0.1
+#    SC₂⁰::Float64 = 0.1
+#    Δ⁰::Int = 0
+#end
+
+# Set the default values of the parameters and the initial conditions
+model_constants = parameters()
+#ini_con = ini_conditions()
+
+# make functions one liners for readability
+sub_asm(a, b, X) = a * X / (b + X)
+dilution(X, Y, Z, a, b, c, d) = a * X + b * Y + c * Z + d
+
+# Define the ODE system, parameter list p can be retrieved from the previous settings
+function ode_sys_tri(du, u, p, t)
+    du[1] =
+        u[6] * dilution(u[3], u[4], u[5], p.pop_c, p.pop_sc_1, p.pop_sc_2, 0) * (p.N₁⁰ - u[1]) -
+        p.c₁ * sub_asm(p.a, p.b, u[1]) * u[3] / p.OD_chlamy -
+        p.c₂ * sub_asm(p.c_1, p.d_1, u[1]) * u[4] / p.OD_bacteria - 
+        p.c₄ * sub_asm(p.c_2, p.d_2, u[1]) * u[5] / p.OD_bacteria
+
+    du[2] =
+        p.α * u[3] -
+        p.c₃ * sub_asm(p.e_1, p.f_1, u[2]) * u[4] / p.OD_bacteria -
+        p.c₅ * sub_asm(p.e_2, p.f_2, u[2]) * u[5] / p.OD_bacteria -
+        u[6] * u[2] * dilution(u[3], u[4], u[5], p.pop_c, p.pop_sc_1, p.pop_sc_2, 0)
+
+    du[3] =
+        u[3] * (
+            p.c₁ * sub_asm(p.a, p.b, u[1]) -
+            u[6] * dilution(u[3], u[4], u[5], p.pop_c, p.pop_sc_1, p.pop_sc_2, 0) - 
+            p.d₁
+        )
+
+    du[4] =
+        u[4] * (
+            p.c₂ * sub_asm(p.c_1, p.d_1, u[1]) +
+            p.c₃ * sub_asm(p.e_1, p.f_1, u[2]) - p.d₂ -
+            u[6] * dilution(u[3], u[4], u[5], p.pop_c, p.pop_sc_1, p.pop_sc_2, 0) +
+            p.β₂ * u[5]
+        )
+
+    du[5] = 
+          u[5] * (
+            p.c₄ * sub_asm(p.c_2, p.d_2, u[1]) +
+            p.c₅ * sub_asm(p.e_2, p.f_2, u[2]) - p.d₃ -
+            u[6] * dilution(u[3], u[4], u[5], p.pop_c, p.pop_sc_1, p.pop_sc_2, 0) +
+            p.β₁ * u[4]
+        )
+
+    du[6] = 0
+end
+
+# Define the stochasity adding to the ODE system
+function σ_ode_sys(du, u, p, t)
+    du[1] = 0.03*u[1]
+    du[2] = 0.05*u[2]
+    du[3] = 0.05*u[3]
+    du[4] = 0.05*u[4]
+    du[5] = 0.05*u[4]
+    du[6] = 0
+end
+
+# Modify the conditions for the dilution switch
+activate_dilution_condition(u, t, integrator) =
+    integrator.u[6] < 1 && integrator.u[3] + integrator.u[4] + integrator.u[5] > integrator.p.threshold
+
+activate_dilution_affect!(integrator) = integrator.u[6] = 1
+
+stop_dilution_condition(u, t, integrator) =
+    integrator.u[6] > 0 && integrator.u[3] + integrator.u[4] + integrator.u[5] < 0.9 * integrator.p.threshold
+
+stop_dilution_affect!(integrator) = integrator.u[6] = 0.0
+
+# Create the callback set with switching conditions
+cb1 = DiscreteCallback(activate_dilution_condition, activate_dilution_affect!)
+cb2 = DiscreteCallback(stop_dilution_condition, stop_dilution_affect!)
+cbs = CallbackSet(cb1, cb2)
+
+# Create a array for initial conditions
+u0 = [
+    model_constants.N₁⁰,
+    model_constants.N₂⁰,
+    model_constants.C⁰,
+    model_constants.SC₁⁰,
+    model_constants.SC₂⁰,
+    model_constants.Δ⁰
+]
+
+# Set the time for simulation
+tspan = (0.0, 1000.0)
+
+# Choose the random process for simulating the SDE system
+W = WienerProcess(0.0,0.0,0.0)
+# OU = OrnsteinUhlenbeckProcess(1.0, 1.2, 0.5, 0.0, 0.0)
+
+prob = SDEProblem(ode_sys_tri, σ_ode_sys, u0, tspan, model_constants, noise=W)
+sol = solve(
+    prob,
+    SRIW1(),
+    callback=cbs,
+    saveat=0.1
+)
+
+plot_layout = @layout [
+    grid(3,2)
+    #a{0.2h}
+]
+
+plot(sol,
+    dpi = 500,
+    size = (600,600),
+    layout = plot_layout,
+    title = ["N₁" "N₂" "Chlamy" "SynCom_1" "SynCom_2" "Dilution Switch"],
+    label = ["" "" "" "" "" ""]
+    )
